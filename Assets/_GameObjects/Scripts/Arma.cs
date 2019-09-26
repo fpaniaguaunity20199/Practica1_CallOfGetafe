c1254199b82f0ed62bd9f51939d2cfb255f3ae6c
Fernando Paniagua <fpaniagua.unity.2018.9@gmail.com>
2019-09-26 14:10:28 +0200

    Initial commit

---
+++ Assets/_GameObjects/Scripts/Arma.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arma : MonoBehaviour
+{
+    [SerializeField] GameObject prefabBala;
+    [SerializeField] Transform transformPuntoDeDisparo;
+    [SerializeField] float fuerzaDisparo;
+
+    public void Disparar()
+    {
+        //1. Instanciar la bala (necesito el prefab de la bala)
+        GameObject bala = Instantiate(prefabBala);
+        //1.5. Posicionar la bala (necesito el punto de spawn)
+        bala.transform.position = transformPuntoDeDisparo.transform.position;
+        bala.transform.rotation = transformPuntoDeDisparo.transform.rotation;
+        //2.Impulsar la bala (necesito el rigidbody de la bala)
+        bala.GetComponent<Rigidbody>().AddForce(0, 0, fuerzaDisparo);
+    }
+}
---
+++ Assets/_GameObjects/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] Arma arma;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Disparar();
+        }
+    }
+
+    void Disparar()
+    {
+        print("DISPARAR");
+        arma.Disparar();
+    }
+
+
+}
