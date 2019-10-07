69c931737d335bf7f66ff20d67553c1efffaca22
Fernando Paniagua <fpaniagua.unity.2018.9@gmail.com>
2019-10-07 13:58:37 +0200

    2019-10-07

--- Assets/_GameObjects/Scripts/Player.cs
+++ Assets/_GameObjects/Scripts/Player.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] Arma arma;
-    private Arma otra;
+    [SerializeField] Arma[] armas;
+    [SerializeField] int armaActiva = 0;
 
     private void Update()
     {
@@ -13,16 +13,52 @@ public class Player : MonoBehaviour
         {
             ApretarGatillo();
         }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ActivarArma(0);
+        } else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ActivarArma(1);
+        } else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            ActivarArma(2);
+        } else if (Input.GetKeyDown("4"))
+        {
+            ActivarArma(3);
+        }
+    }
+
+    void ActivarArma(int numeroArma)
+    {
+        if (numeroArma != armaActiva)
+        {
+            //OPCION 1
+
+            armas[armaActiva].gameObject.SetActive(false);
+            armaActiva = numeroArma;
+            armas[armaActiva].gameObject.SetActive(true);
+
+            //OPCION 2
+
+            /*
+            armaActiva = numeroArma;
+            for(int i = 0; i < armas.Length; i++)
+            {
+                armas[i].gameObject.SetActive(false);
+            }
+            armas[armaActiva].gameObject.SetActive(true);
+            */
+        }
     }
 
     public void Recargar(int numeroBalas)
     {
-        arma.Recargar(numeroBalas);
+        //arma.Recargar(numeroBalas);
     }
 
 
     void ApretarGatillo()
     {
-        arma.Disparar();
+        //armas[armaActiva].Disparar();
     }
 }
---
+++ Assets/_GameObjects/Scripts/Weapons/Arma.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arma : MonoBehaviour
+{
+    [Space(30)]
+    [Header("Tiempo entre disparos(segundos)")]
+    [Range(0,10)]
+    [SerializeField] float cadencia;//Tiempo entre disparos (segundos)
+    [Header("Capacidad de proyectiles en un cargador")]
+    [SerializeField] int capacidadCargador;
+    [Header("Tiempo de recarga (segundos)")]
+    [Range(0, 4)]
+    [SerializeField] float tiempoRecarga;
+    [Header("Número de cargadores en la mochila")]
+    [SerializeField] int numeroCargadores;
+    //Munición disponible en el cargador actual
+    private int municionCargador;
+
+    public void IntentarDisparar()
+    {
+
+    }
+
+    private void Disparar()
+    {
+
+    }
+    public void Recargar()
+    {
+    }
+    public void IncrementarCargador(int numeroCargadores)
+    {
+        this.numeroCargadores = this.numeroCargadores + numeroCargadores;
+    }
+}
