0cbd491098c2a4813bcc4982740113de539b22e0
Fernando Paniagua <fpaniagua.unity.2018.9@gmail.com>
2019-10-11 13:59:57 +0200

    2019-10-11

--- Assets/_GameObjects/Scripts/Weapons/ArmaSnipper.cs
+++ Assets/_GameObjects/Scripts/Weapons/ArmaSnipper.cs
@@ -6,14 +6,17 @@ public class ArmaSnipper : Arma
 {
     [Header("Zoom")]
     [SerializeField] Camera camara;
+    [SerializeField] Canvas canvas;
     private void Update()
     {
         if (Input.GetMouseButtonDown(1))
         {
             camara.fieldOfView = 20;
+            canvas.gameObject.SetActive(true);
         } else if (Input.GetMouseButtonUp(1))
         {
             camara.fieldOfView = 60;
+            canvas.gameObject.SetActive(false);
         }
     }
 
