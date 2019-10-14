315fe3df78496f25c2764ddcbd33225df34e1f57
Fernando Paniagua <fpaniagua.unity.2018.9@gmail.com>
2019-10-14 14:05:47 +0200

    14-10-2019

---
+++ Assets/_GameObjects/Scripts/Enemies/EnemigoPesado.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemigoPesado : MonoBehaviour
+{
+    private Transform player;
+    public float speed;
+    void Start()
+    {
+        player = GameObject.Find("Player").transform;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, player.position, speed);
+    }
+}
---
+++ Assets/_GameObjects/Scripts/Enemies/EnemigoTorre.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemigoTorre : Enemy
+{
+    [SerializeField] float attackDistance;
+    [SerializeField] float speed;
+    private float distanceToPlayer;
+
+
+    private void Update()
+    {
+        base.Update();
+        distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+        if (distanceToPlayer < attackDistance)
+        {
+            Quaternion q = Quaternion.LookRotation(
+                player.transform.position - 
+                transform.position);
+            transform.rotation = Quaternion.RotateTowards(
+                transform.rotation, 
+                q, 
+                Time.deltaTime * speed);
+        }
+    }
+
+}
